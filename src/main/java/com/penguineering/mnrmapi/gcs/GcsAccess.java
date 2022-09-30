60d644d60e1d9c47c29a6bfc22ff4132f43436cf
Stefan Haun <mail@tuxathome.de>
2022-09-30 15:44:04 +0200

    :sparkles: Add accessor Bean for GCS (Google Cloud Services)
    Currently allows to lookup URLs from IDs and retrieve data from a URL.

---
+++ src/main/java/com/penguineering/mnrmapi/gcs/GcsAccess.java
@@ -0,0 +1,58 @@
+package com.penguineering.mnrmapi.gcs;
+
+import com.penguineering.mnrmapi.auth.Session;
+import com.penguineering.mnrmapi.discovery.ServiceDiscovery;
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import jakarta.inject.Inject;
+import reactor.core.publisher.Flux;
+
+import java.net.URI;
+
+import static io.micronaut.http.HttpHeaders.ACCEPT;
+
+@Bean
+public class GcsAccess {
+    @Inject
+    protected HttpClient httpClient;
+
+    @Inject
+    protected Session session;
+
+    @Inject
+    protected ServiceDiscovery discovery;
+
+    /**
+     * Transform a GCS path to a storage URL
+     * @param gcsPath Mono with GCS path as String
+     * @return Mono with storage URI
+     */
+    public Flux<URI> lookup(Flux<String> gcsPath) {
+        return gcsPath
+                .map(FileRequestBody::withGet)
+                .zipWith(
+                        // Repeat this result, but not the look-up
+                        discovery.fetchDocumentAPI().repeat(),
+                        (body, uri) -> HttpRequest
+                                .POST(uri, body)
+                                .header(ACCEPT, "application/json"))
+                .transform(session::userAuthenticatedRequest)
+                .map(req -> httpClient.retrieve(req, FileRequestResponse.class))
+                .flatMap(Flux::from)
+                .map(FileRequestResponse::getUrl);
+    }
+
+    /**
+     * Retrieve the content from a GCS path after lookup
+     * @param gcsPath Mono with GCS path as String
+     * @return downloaded content from this path
+     */
+    public Flux<String> retrieve(Flux<String> gcsPath) {
+        return gcsPath
+                .transform(this::lookup)
+                .map(HttpRequest::GET)
+                .map(httpClient::retrieve)
+                .flatMap(Flux::from);
+    }
+}
